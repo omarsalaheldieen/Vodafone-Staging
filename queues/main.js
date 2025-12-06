0a9477e9827b7ba7b01ed3dbb68be6623e643f7a
omar <omarsalah56er@gmail.com>
2025-12-06 03:39:10 +0200

    Update queue design

--- queues/main.js
+++ queues/main.js
@@ -1,190 +1,56 @@
-
-
 let queueList = [];
 
-socket.on("MKA:Enter", (data) => {
-    let parsedData = JSON.parse(data);
-    let newQueueNumber = parsedData.queueNumber;
+const BASE_URL = "https://arena-backend-staging.gamersloungeme.app/";
 
-    queueList.unshift(newQueueNumber);
+const events = [
+  "MKA:Enter",
+  "TJ:Enter",
+  "TC3:Enter",
+  "SF:Enter",
+  "SI:Enter",
+  "PM:Enter",
+  "CTR:Enter",
+  "MSX:Enter",
+  "TT:Enter",
+  "T8:Enter",
+  "FC:Enter",
+  "CTRF:Enter",
+];
+
+events.forEach((evt) => {
+  socket.on(evt, (data) => {
+    let parsedData = JSON.parse(data);
+    console.log(parsedData);
+
+    let newQueueNumber = parsedData.queueNumber;
+    let gameLogo = parsedData.gameLogo;
+
+    queueList.unshift({
+      queueNumber: newQueueNumber,
+      gameLogo: gameLogo,
+    });
 
     if (queueList.length > 15) {
-        queueList.pop();
+      queueList.pop();
     }
 
     renderQueue();
-});
-
-socket.on("TJ:Enter", (data) => {
-    let parsedData = JSON.parse(data);
-    let newQueueNumber = parsedData.queueNumber;
-
-    queueList.unshift(newQueueNumber);
-
-    if (queueList.length > 15) {
-        queueList.pop();
-    }
-
-    renderQueue();
-});
-
-socket.on("TC3:Enter", (data) => {
-    let parsedData = JSON.parse(data);
-    let newQueueNumber = parsedData.queueNumber;
-
-    queueList.unshift(newQueueNumber);
-
-    if (queueList.length > 15) {
-        queueList.pop();
-    }
-
-    renderQueue();
-});
-
-socket.on("SF:Enter", (data) => {
-    let parsedData = JSON.parse(data);
-    let newQueueNumber = parsedData.queueNumber;
-
-    queueList.unshift(newQueueNumber);
-
-    if (queueList.length > 15) {
-        queueList.pop();
-    }
-
-    renderQueue();
-});
-
-socket.on("SI:Enter", (data) => {
-    let parsedData = JSON.parse(data);
-    let newQueueNumber = parsedData.queueNumber;
-
-    queueList.unshift(newQueueNumber);
-
-    if (queueList.length > 15) {
-        queueList.pop();
-    }
-
-    renderQueue();
-});
-
-socket.on("PM:Enter", (data) => {
-    let parsedData = JSON.parse(data);
-    let newQueueNumber = parsedData.queueNumber;
-
-    queueList.unshift(newQueueNumber);
-
-    if (queueList.length > 15) {
-        queueList.pop();
-    }
-
-    renderQueue();
-});
-
-socket.on("CTR:Enter", (data) => {
-    let parsedData = JSON.parse(data);
-    let newQueueNumber = parsedData.queueNumber;
-
-    queueList.unshift(newQueueNumber);
-
-    if (queueList.length > 15) {
-        queueList.pop();
-    }
-
-    renderQueue();
-});
-
-socket.on("MSX:Enter", (data) => {
-    let parsedData = JSON.parse(data);
-    let newQueueNumber = parsedData.queueNumber;
-
-    queueList.unshift(newQueueNumber);
-
-    if (queueList.length > 15) {
-        queueList.pop();
-    }
-
-    renderQueue();
-});
-
-socket.on("TT:Enter", (data) => {
-    let parsedData = JSON.parse(data);
-    let newQueueNumber = parsedData.queueNumber;
-
-    queueList.unshift(newQueueNumber);
-
-    if (queueList.length > 15) {
-        queueList.pop();
-    }
-
-    renderQueue();
-});
-
-socket.on("T8:Enter", (data) => {
-    let parsedData = JSON.parse(data);
-    let newQueueNumber = parsedData.queueNumber;
-
-    queueList.unshift(newQueueNumber);
-
-    if (queueList.length > 15) {
-        queueList.pop();
-    }
-
-    renderQueue();
-});
-
-socket.on("FC:Enter", (data) => {
-    let parsedData = JSON.parse(data);
-    let newQueueNumber = parsedData.queueNumber;
-
-    queueList.unshift(newQueueNumber);
-
-    if (queueList.length > 15) {
-        queueList.pop();
-    }
-
-    renderQueue();
-});
-
-socket.on("CTRF:Enter", (data) => {
-    let parsedData = JSON.parse(data);
-    let newQueueNumber = parsedData.queueNumber;
-
-    queueList.unshift(newQueueNumber);
-
-    if (queueList.length > 15) {
-        queueList.pop();
-    }
-
-    renderQueue();
+  });
 });
 
 function renderQueue() {
-    const container = document.getElementById("queueContainer");
-    container.innerHTML = "";
+  const container = document.getElementById("queueContainer");
+  container.innerHTML = "";
 
-    queueList.forEach(number => {
-        const div = document.createElement("div");
-        div.className = "queue-item";
-        div.innerHTML = `
-            <div class="ticket">
-                ${number}
-            </div>
+  queueList.forEach((person) => {
+    const div = document.createElement("div");
+    div.className = "queue-item";
+
+    div.innerHTML = `
+            <img src="${BASE_URL}${person.gameLogo}" alt="team" class="team-logo" />
+            <div class="ticket">${person.queueNumber}</div>
         `;
-        container.appendChild(div);
-    });
+
+    container.appendChild(div);
+  });
 }
-
-// const events = [
-//   "G2:Call", "LOL:Call", "FC:Call", "CS:Call",
-//   "PG:Call", "TIA:Call", "TS:Call", "SARA:Call",
-//   "SARAA:Call", "Q1:Call", "STT:Call", "ss:Call",
-//   "2XKO:Call", "STM:Call"
-// ];
-
-// events.forEach(evt => {
-//     socket.on(evt, (data) => {
-//         const parsed = safeParse(data);
-//         console.log(evt, parsed);
-//         addToQueue(parsed);
-//     });
-// });
